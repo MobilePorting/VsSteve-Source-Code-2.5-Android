a5c9b92789c7137c6dc4bf863883cf89eb9dd275
91ebagacm <59393483+91ebagacm@users.noreply.github.com>
2022-10-18 19:47:21 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2648,6 +2648,13 @@ babyArrow.animation.play('static');
 babyArrow.x += 50;
 babyArrow.x += ((FlxG.width / 2) * player);
 
+if (FlxG.save.data.middleScroll) {
+babyArrow.x -= 275;
+if (player != 1) {
+babyArrow.visible = false;
+}
+}
+
 cpuStrums.forEach(function(spr: FlxSprite) {
 spr.centerOffsets(); //CPU arrows start out slightly off-center
 });

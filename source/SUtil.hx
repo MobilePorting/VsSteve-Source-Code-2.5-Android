c8c6a14706e1afbc98ff566f53a7255364492dca
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-10-18 22:09:38 +0300

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,31 +1,73 @@
 package;
 
 #if android
-import android.Hardware;
 import android.Permissions;
-import android.os.Build.VERSION;
+import android.os.Build;
 import android.os.Environment;
+import android.widget.Toast;
 #end
 import flash.system.System;
 import flixel.FlxG;
-import flixel.util.FlxStringUtil;
 import haxe.CallStack.StackItem;
 import haxe.CallStack;
 import haxe.io.Path;
-import lime.app.Application;
-import openfl.events.UncaughtErrorEvent;
-import openfl.utils.Assets as OpenFlAssets;
 import openfl.Lib;
+import openfl.events.UncaughtErrorEvent;
+import openfl.utils.Assets;
+#if (sys && !ios)
 import sys.FileSystem;
 import sys.io.File;
+#end
+
+using StringTools;
 
 /**
  * ...
- * @author: Saw (M.A. Jigsaw)
- * @modified: mcagabe19
+ * @author Mihai Alexandru (M.A. Jigsaw)
  */
 class SUtil
 {
+	/**
+	 * A simple function that checks for storage permissions and game files/folders
+	 */
+	public static function check()
+	{
+		#if android
+		if (!Permissions.getGrantedPermissions().contains(Permissions.WRITE_EXTERNAL_STORAGE)
+			&& !Permissions.getGrantedPermissions().contains(Permissions.READ_EXTERNAL_STORAGE))
+		{
+			if (VERSION.SDK_INT >= VERSION_CODES.M)
+			{
+				Permissions.requestPermissions([Permissions.WRITE_EXTERNAL_STORAGE, Permissions.READ_EXTERNAL_STORAGE]);
+
+				/**
+				 * Basically for now i can't force the app to stop while its requesting a android permission, so this makes the app to stop while its requesting the specific permission
+				 */
+				Lib.application.window.alert('You needed grant storage permission for save crash logs'
+					+ '\nPress Ok to contiune',
+					'Permissions?');
+			}
+			else
+			{
+				Lib.application.window.alert('Please grant the game storage permissions in app settings' + '\nPress Ok to close the app', 'Permissions?');
+				System.exit(1);
+			}
+		}
+
+		if (Permissions.getGrantedPermissions().contains(Permissions.WRITE_EXTERNAL_STORAGE)
+			&& Permissions.getGrantedPermissions().contains(Permissions.READ_EXTERNAL_STORAGE))
+		{
+			if (!FileSystem.exists(SUtil.getPath()))
+				FileSystem.createDirectory(SUtil.getPath());
+		}
+		#end
+	}
+
+	/**
+	 * This returns the external storage path that the game will use
+	 */
+	public static function getPath():String #if android return Environment.getExternalStorageDirectory() + '/' + '.' + Lib.application.meta.get('file') +
+		'/'; #else return ''; #end
 
 	/**
 	 * Uncaught error handler, original made by: sqirra-rng
@@ -41,26 +83,55 @@ class SUtil
 			{
 				switch (stackItem)
 				{
+					case CFunction:
+						errMsg += 'a C function\n';
+					case Module(m):
+						errMsg += 'module ' + m + '\n';
 					case FilePos(s, file, line, column):
 						errMsg += file + ' (line ' + line + ')\n';
-					default:
-						Sys.println(stackItem);
+					case Method(cname, meth):
+						errMsg += cname == null ? "<unknown>" : cname + '.' + meth + '\n';
+					case LocalFunction(n):
+						errMsg += 'local function ' + n + '\n';
 				}
 			}
 
 			errMsg += u.error;
 
-			Sys.println(errMsg);
-			Application.current.window.alert(errMsg, 'Error!');
-
+			#if (sys && !ios)
 			try
-			{}
-			catch (e:Dynamic)
-                                #if android
-				Hardware.toast("Error!\nClouldn't save the crash dump because:\n" + e, 2);
-                                #end
+			{
+				if (!FileSystem.exists(SUtil.getPath() + 'logs'))
+					FileSystem.createDirectory(SUtil.getPath() + 'logs');
 
+				File.saveContent(SUtil.getPath()
+					+ 'logs/'
+					+ Lib.application.meta.get('file')
+					+ '-'
+					+ Date.now().toString().replace(' ', '-').replace(':', "'")
+					+ '.log',
+					errMsg
+					+ '\n');
+			}
+			#if android
+			catch (e:Dynamic)
+			Toast.makeText("Error!\nClouldn't save the crash dump because:\n" + e, Toast.LENGTH_LONG);
+			#end
+			#end
+
+			println(errMsg);
+			Lib.application.window.alert(errMsg, 'Error!');
 			System.exit(1);
 		});
+	}
+
+	private static function println(msg:String):Void
+	{
+		#if sys
+		Sys.println(msg);
+		#else
+		// Pass null to exclude the position.
+		haxe.Log.trace(msg, null);
+		#end
 	}
 }

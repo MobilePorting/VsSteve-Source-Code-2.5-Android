799bfab5c81b48fba5697c790255b3d98bba179e
91ebagacm <59393483+91ebagacm@users.noreply.github.com>
2022-11-02 10:51:13 +0300

    Update FlxVirtualPad.hx

--- source/mobile/flixel/FlxVirtualPad.hx
+++ source/mobile/flixel/FlxVirtualPad.hx
@@ -1,12 +1,13 @@
 package mobile.flixel;
 
+import mobile.flixel.FlxButton;
 import flixel.FlxG;
-import flixel.graphics.FlxGraphic;
-import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.frames.FlxTileFrames;
 import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxPoint;
-import mobile.flixel.FlxButton;
+import flixel.util.FlxDestroyUtil;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.graphics.FlxGraphic;
 import openfl.utils.Assets;
 
 enum FlxDPadMode
@@ -197,7 +198,7 @@ class FlxVirtualPad extends FlxSpriteGroup
 		button.immovable = true;
 		button.scrollFactor.set();
 		button.color = Color;
-		button.alpha = 0.6;
+		button.alpha = MobileControls.getOpacity(false);
 		#if FLX_DEBUG
 		button.ignoreDrawDebug = true;
 		#end

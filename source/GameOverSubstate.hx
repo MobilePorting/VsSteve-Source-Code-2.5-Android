1a03c0698a0a683c592bd2f7cc248bb992ed9812
91ebagacm <59393483+91ebagacm@users.noreply.github.com>
2022-11-02 21:01:57 +0300

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -18,6 +18,9 @@ class GameOverSubstate extends MusicBeatSubstate
 
 	public function new(x:Float, y:Float)
 	{
+                Paths.clearUnusedMemory();
+                Paths.clearStoredMemory();
+
 		var daStage = PlayState.curStage;
 		var daBf:String = '';
 		switch (PlayState.SONG.player1)
